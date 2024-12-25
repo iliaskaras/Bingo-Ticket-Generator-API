938ae3718902d6d93fbd80c3689fcdb9ef79d506
iliaskaras <hlias.karas@yahoo.gr>
2024-12-25 21:23:54 +0200

    Update dependencies
    1. Add spring-boot-starter-validation (for enabling jakarta validations)
    2. Add springdoc-openapi-starter-webmvc-ui (for enabling swagger schemas)
    3. Remove security (to remove the need of security since this is a simple application)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.spockframework:spock-core:2.3-groovy-3.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testImplementation("org.codehaus.groovy:groovy-all:3.0.9")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
